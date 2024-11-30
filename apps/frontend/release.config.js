bd9b1294d1f75e802a85a44bd5a3a6bf4ce3709e
jagadhis <jwaran78@gmail.com>
2024-12-01 03:33:36 +0530

    feat: ready for first release

---
+++ apps/frontend/release.config.js
@@ -0,0 +1,15 @@
+/** @type {import('semantic-release').GlobalConfig} */
+const config = {
+  branches: [
+    'master',
+    { name: 'beta', prerelease: true }
+  ],
+  plugins: [
+    '@semantic-release/commit-analyzer',
+    '@semantic-release/release-notes-generator',
+    '@semantic-release/npm',
+    '@semantic-release/github'
+  ]
+}
+
+module.exports = config
