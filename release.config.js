2e9b1e05c24414ebbe369c21e489f068f1297635
jagadhis <jwaran78@gmail.com>
2024-12-01 11:18:51 +0530

    build: publishing to npm and github registry

--- release.config.js
+++ release.config.js
@@ -12,9 +12,6 @@ module.exports = {
     ['@semantic-release/git', {
       assets: ['package.json'],
       message: 'chore(release): ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}'
-    }],
-    ['@semantic-release/exec', {
-      successCmd: 'npm publish --registry https://npm.pkg.github.com --tag github-${nextRelease.version}'
     }]
   ]
 };
