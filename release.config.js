a5a08e2880cbbdf30fac43fd2a186fe12db9860a
jagadhis <jwaran78@gmail.com>
2024-12-01 17:22:41 +0530

    feat: allow create directory from npx command

--- release.config.js
+++ release.config.js
@@ -3,21 +3,23 @@ module.exports = {
   plugins: [
     '@semantic-release/commit-analyzer',
     '@semantic-release/release-notes-generator',
-    ['@semantic-release/npm', {
-      npmPublish: true,
-      tarballDir: 'dist',
-      pkgRoot: '.',
-      registry: 'https://registry.npmjs.org/',
-      tag: 'latest'
-    }],
-    ['@semantic-release/github', {
-      assets: [
-        { path: 'dist/*.tgz', label: 'npm package' }
-      ]
-    }],
-    ['@semantic-release/git', {
-      assets: ['package.json'],
-      message: 'chore(release): ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}'
-    }]
+    [
+      '@semantic-release/npm',
+      {
+        npmPublish: true,
+        tarballDir: 'dist',
+        pkgRoot: '.',
+        registry: 'https://registry.npmjs.org/',
+        tag: 'latest'
+      }
+    ],
+    '@semantic-release/github',
+    [
+      '@semantic-release/git',
+      {
+        assets: ['package.json'],
+        message: 'chore(release): ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}'
+      }
+    ]
   ]
 };
